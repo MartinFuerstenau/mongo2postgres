982aba77359f639bb4978263322fefa7568a8fbb
Martin Fürstenau <martin.fuerstenau@freenet.de>
2019-02-22 21:48:50 +0100

    Make it work

--- src/main/java/demo/HerosRepository.java
+++ src/main/java/demo/HerosRepository.java
@@ -1,17 +1,16 @@
 package demo;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 public interface HerosRepository extends JpaRepository<HeroEntity, Long> {
 
-    @Query( value = "SELECT * FROM heros WHERE hero ->> 'name' = ?1", nativeQuery = true )
-    List<HeroEntity> findAllByName( String name );
+	@Query(value = "SELECT * FROM heros WHERE hero ->> 'name' = ?1", nativeQuery = true)
+	HeroEntity findAllByName(String name);
 
-    @Query( value = "SELECT * FROM heros WHERE hero ->> 'superpowers' n#> ?1", nativeQuery = true )
-    List<HeroEntity> findAllBySuperpower( String superpower );
+	@Query(value = "SELECT * FROM heros WHERE hero -> 'superpowers' @> to_jsonb(:superpower)", nativeQuery = true)
+	List<HeroEntity> findAllBySuperpower(@Param("superpower") String superpower);
 }
--- src/test/java/demo/Mongo2postgresApplicationTests.java
+++ src/test/java/demo/Mongo2postgresApplicationTests.java
@@ -1,11 +1,12 @@
 package demo;
 
-
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,74 +21,82 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 import demo.Hero.UNIVERSE;
 
-
-@RunWith( SpringRunner.class )
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@ContextConfiguration( initializers = { Mongo2postgresApplicationTests.Initializer.class } )
+@ContextConfiguration(initializers = { Mongo2postgresApplicationTests.Initializer.class })
 public class Mongo2postgresApplicationTests {
 
-    @ClassRule
-    public static PostgreSQLContainer postgreSQLContainer = (PostgreSQLContainer) new PostgreSQLContainer( "postgres:10.4" )
-            .withDatabaseName( "sampledb" ).withUsername( "sampleuser" ).withPassword( "samplepwd" )
-            .withStartupTimeout( Duration.ofSeconds( 600 ) );
+	@ClassRule
+	public static PostgreSQLContainer postgreSQLContainer = (PostgreSQLContainer) new PostgreSQLContainer(
+			"postgres:10.4").withDatabaseName("sampledb").withUsername("sampleuser").withPassword("samplepwd")
+					.withStartupTimeout(Duration.ofSeconds(600));
 
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-        @Override
-        public void initialize( ConfigurableApplicationContext configurableApplicationContext ) {
-            System.out.println( "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl() + "\nspring.datasource.username="
-                                + postgreSQLContainer.getUsername() + "\nspring.datasource.password=" + postgreSQLContainer.getPassword() );
+		@Override
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			System.out.println("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl()
+					+ "\nspring.datasource.username=" + postgreSQLContainer.getUsername()
+					+ "\nspring.datasource.password=" + postgreSQLContainer.getPassword());
 
-            TestPropertyValues
-                    .of( "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                         "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                         "spring.datasource.password=" + postgreSQLContainer.getPassword() )
-                    .applyTo( configurableApplicationContext.getEnvironment() );
-        }
-    }
+			TestPropertyValues
+					.of("spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+							"spring.datasource.username=" + postgreSQLContainer.getUsername(),
+							"spring.datasource.password=" + postgreSQLContainer.getPassword())
+					.applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 
-    @Autowired
-    HerosRepository repo;
+	@Autowired
+	HerosRepository repo;
 
-    @Test
-    public void contextLoads() {
+	@Before
+	public void setup() {
+		var superman = new Hero();
+		superman.setName("Superman");
+		superman.setAlterEgo(new AlterEgo("Clark Kent", "Jounalist"));
+		superman.setUniverse(UNIVERSE.DC);
+		superman.setSuperpowers(Set.of("X-Ray Vision", "Super-Breath", "Heat Vision", "Super-Speed", "Flight",
+				"Super-Strength", "Invulnerability"));
+		repo.saveAndFlush(new HeroEntity(superman));
 
-        var superman = new Hero();
-        superman.setName( "Superman" );
-        superman.setAlterEgo( new AlterEgo( "Clark Kent", "Jounalist" ) );
-        superman.setUniverse( UNIVERSE.DC );
-        superman.setSuperpowers( Set
-                .of( "X-Ray Vision", "Super-Breath", "Heat Vision", "Super-Speed", "Flight", "Super-Strength", "Invulnerability" ) );
-        repo.saveAndFlush( new HeroEntity( superman ) );
+		var batman = new Hero();
+		batman.setName("Batman");
+		batman.setAlterEgo(new AlterEgo("Bruce Wayne", "Billionaire"));
+		batman.setUniverse(UNIVERSE.DC);
+		batman.setSuperpowers(
+				Set.of("X-Ray Vision", "Genius Intellect", "Invisibility", "Flight", "Will-Power", "Reflexes"));
 
-        var batman = new Hero();
-        batman.setName( "Batman" );
-        batman.setAlterEgo( new AlterEgo( "Bruce Wayne", "Billionaire" ) );
-        batman.setUniverse( UNIVERSE.DC );
-        batman.setSuperpowers( Set.of( "X-Ray Vision", "Genius Intellect", "Invisibility", "Flight", "Will-Power", "Reflexes" ) );
+		var ironman = new Hero();
+		ironman.setName("Iron Man");
+		ironman.setAlterEgo(new AlterEgo("Toni Stark", "Genious"));
+		ironman.setUniverse(UNIVERSE.MARVEL);
+		ironman.setSuperpowers(Set.of("Direct Cybernetic Interface", "Wireless Communication", "Reflexes",
+				"Regenerative Systems", "Minor Shapeshifting", "Billionaire", "Playboy"));
 
-        var ironman = new Hero();
-        ironman.setName( "Iron Man" );
-        ironman.setAlterEgo( new AlterEgo( "Toni Stark", "Genious" ) );
-        ironman.setUniverse( UNIVERSE.MARVEL );
-        ironman.setSuperpowers( Set.of( "Direct Cybernetic Interface",
-                                        "Wireless Communication",
-                                        "Reflexes",
-                                        "Regenerative Systems",
-                                        "Minor Shapeshifting",
-                                        "Billionaire",
-                                        "Playboy" ) );
+		repo.saveAll(Arrays.asList(new HeroEntity(batman), new HeroEntity(ironman)));
+	}
 
-        repo.saveAll( Arrays.asList( new HeroEntity( batman ), new HeroEntity( ironman ) ) );
+	@After
+	public void teardown() {
+		repo.deleteAll();
+	}
 
-        List<HeroEntity> findAll = repo.findAll();
-        findAll.forEach( System.out::println );
+	@Test
+	public void findAll() {
+		List<HeroEntity> findAll = repo.findAll();
+		findAll.forEach(System.out::println);
+	}
 
-        List<HeroEntity> findAllByName = repo.findAllByName( "Iron Man" );
-        findAllByName.forEach( System.out::println );
+	@Test
+	public void findByName() {
+		HeroEntity findByName = repo.findAllByName("Iron Man");
+		System.out.println(findByName);
+	}
 
-        List<HeroEntity> findAllBySuperpower = repo.findAllBySuperpower( "Reflexes" );
-        findAllBySuperpower.forEach( System.out::println );
-    }
-
+	@Test
+	public void findBySuperpower() {
+		List<HeroEntity> findAllBySuperpower = repo.findAllBySuperpower("Reflexes");
+		findAllBySuperpower.forEach(System.out::println);
+	}
 }
