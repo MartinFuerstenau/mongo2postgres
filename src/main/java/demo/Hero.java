bd57ddf3fb9f979c6f68011eeb38a985ed453a49
Martin Fürstenau <martin.fuerstenau@freenet.de>
2019-03-10 08:33:52 +0100

    removed unnecessary field configured to use with swarm overlay network

--- src/main/java/demo/Hero.java
+++ src/main/java/demo/Hero.java
@@ -1,22 +1,18 @@
 package demo;
 
-
 import java.util.Set;
 
 import lombok.Data;
 
-
 @Data
 public class Hero {
 
-    enum UNIVERSE {
-        DC,
-        MARVEL
-    }
+	enum UNIVERSE {
+		DC, MARVEL
+	}
 
-    private String name;
-    private String superpower;
-    private Set<String> superpowers;
-    private AlterEgo alterEgo;
-    private UNIVERSE universe;
+	private String name;
+	private Set<String> superpowers;
+	private AlterEgo alterEgo;
+	private UNIVERSE universe;
 }
