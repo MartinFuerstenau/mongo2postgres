45c0b4c81d7fa2e48da5e4b34969cfe5303100b4
Martin Fürstenau <martin.fuerstenau@ratepay.com>
2020-01-15 17:27:30 +0100

    merge conflict

--- src/main/java/demo/Hero.java
+++ src/main/java/demo/Hero.java
@@ -1,18 +1,21 @@
 package demo;
 
+
 import java.util.Set;
 
 import lombok.Data;
 
+
 @Data
 public class Hero {
 
-	enum UNIVERSE {
-		DC, MARVEL
-	}
+    enum UNIVERSE {
+        DC,
+        MARVEL
+    }
 
-	private String name;
-	private Set<String> superpowers;
-	private AlterEgo alterEgo;
-	private UNIVERSE universe;
+    private String name;
+    private Set<String> superpowers;
+    private AlterEgo alterEgo;
+    private UNIVERSE universe;
 }
