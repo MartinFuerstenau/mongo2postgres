406935e22304b5d54c37bf0eff59ae59a666503f
Martin Fürstenau <martin.fuerstenau@ratepay.com>
2020-01-15 18:24:05 +0100

    spring boot actuators

--- src/main/java/demo/Mongo2postgresApplication.java
+++ src/main/java/demo/Mongo2postgresApplication.java
@@ -1,14 +1,15 @@
 package demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Mongo2postgresApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Mongo2postgresApplication.class, args);
-	}
+    public static void main( String[] args ) {
+        SpringApplication.run( Mongo2postgresApplication.class, args );
+    }
 
 }
-
